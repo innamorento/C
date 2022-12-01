aa0b40183b3af89cd0b78848237ac118b7f5d1db
innamorento <innamorento@gmail.com>
2022-12-01 20:33:43 +0300

    Добаавлено решение первой задачи с ответом является ли второе число квадратом первого

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int a,b;
+Console.WriteLine("Введите 2 числа");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a*a == b)
+{
+    Console.WriteLine(b + " является квадртаом " + a);
+
+}
+else
+{
+    Console.WriteLine(b + " НЕ является квадртаом " + a);    
+}
