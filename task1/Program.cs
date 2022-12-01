fe1c13a732d107a8fe9f9ac62fd15c637d2a2a3c
innamorento <innamorento@gmail.com>
2022-12-01 20:45:59 +0300

    Добаавлено решение второй задачи с названием дня недели

--- task1/Program.cs
+++ task1/Program.cs
@@ -5,12 +5,12 @@ Console.WriteLine("Введите 2 числа");
 a = Convert.ToInt32(Console.ReadLine());
 b = Convert.ToInt32(Console.ReadLine());
 
-if (a*a == b)
+if (a == b*b)
 {
-    Console.WriteLine(b + " является квадртаом " + a);
+    Console.WriteLine(a + " является квадртаом " + b);
 
 }
 else
 {
-    Console.WriteLine(b + " НЕ является квадртаом " + a);    
+    Console.WriteLine(a + " НЕ является квадртаом " + b);    
 }
