95d08995ced0f349d2ffcd307f65f3a3bf9d9615
innamorento <innamorento@gmail.com>
2022-12-03 19:07:50 +0300

    Добаавлено решение задачи с определением кратности одного числа другому и вывода остатка от деления

---
+++ tast12/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result = a % b;
+if (result ==0) 
+{
+    Console.WriteLine($"{a} кратно {b}");    
+}
+else
+{
+    Console.WriteLine($"{a} не кратно {b}, остаток {result}");
+}
