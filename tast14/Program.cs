42887e4b2f4689ee3498e1d883c1abe1dc31c9ff
innamorento <innamorento@gmail.com>
2022-12-03 19:12:32 +0300

    Добаавлено решение задачи с определением кратности числа и 7 и 23

---
+++ tast14/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int thirdnumber = 7;
+int secondnumber = 23;
+
+if (a % thirdnumber==0 & a % secondnumber==0) 
+{
+    Console.WriteLine("ДА");    
+}
+else
+{
+    Console.WriteLine("НЕТ");  
+}
