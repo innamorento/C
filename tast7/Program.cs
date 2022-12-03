d180618714ae86c4b33f1a28d3188f30a2b8a016
innamorento <innamorento@gmail.com>
2022-12-03 09:46:59 +0300

    Добаавлено решение задачи с определением четности числа

---
+++ tast7/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a,remdevision;
+Console.WriteLine("Введите число");
+a = Convert.ToInt32(Console.ReadLine());
+remdevision = a%2;
+if (remdevision == 0)
+{
+   Console.WriteLine("Число четное"); 
+}
+else
+{
+    Console.WriteLine("Число не четное"); 
+}
+
