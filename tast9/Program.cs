e0d8b56106d176246b306774bcbdc0a4880c7478
innamorento <innamorento@gmail.com>
2022-12-03 18:41:57 +0300

    Добаавлено решение задачи с определением максимумальной цифры у двухзначного числа

---
+++ tast9/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = new Random().Next(10,100);
+
+int secondnumber = n%10;
+int firstnumber = n/10;
+if (firstnumber > secondnumber) 
+{
+    Console.WriteLine(firstnumber);
+}
+else
+{
+    Console.WriteLine(secondnumber) ;
+}
