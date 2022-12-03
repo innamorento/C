172976a995b7f27f09f5f97ae51dca6463173fde
innamorento <innamorento@gmail.com>
2022-12-03 09:34:28 +0300

    Добаавлено решение задачи с определением максимума 2 чисел

---
+++ tast5/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a,b;
+Console.WriteLine("Введите 2 числа");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine(a + " больше" + b);
+
+}
+else if (a == b)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    Console.WriteLine(b + " больше " + a);    
+}
