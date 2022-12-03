7605f54726e0858684af4a1c68cea5dce0d49337
innamorento <innamorento@gmail.com>
2022-12-03 09:53:46 +0300

    Добаавлено решение задачи с выводом всех четных чисел в диапазоне от 1 до a

---
+++ tast8/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a,remdevision;
+Console.WriteLine("Введите число");
+a = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= a; i++)
+{
+    remdevision = i % 2;
+    if (remdevision == 0)
+    {
+        Console.Write (i + " ");
+    }
+}
+
+
+
