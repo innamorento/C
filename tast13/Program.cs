df183a7f225be3d050fb93eac132c4496cb877b2
innamorento <innamorento@gmail.com>
2022-12-03 19:29:12 +0300

    Добаавлено решение задачи с выводом 3 цифры и определением есть ли она

---
+++ tast13/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int thirdnumber = (n/100)%10;
+
+if (n <100)
+{
+    Console.WriteLine("Третьего числа нет");
+}
+else
+{
+   Console.WriteLine(thirdnumber); 
+}
+
+
