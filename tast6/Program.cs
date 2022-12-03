f3aaf7b915c3416e104a2e2c4bc5ebc7f636159b
innamorento <innamorento@gmail.com>
2022-12-03 09:39:25 +0300

    Добаавлено решение задачи с определением максимума 3 чисел

---
+++ tast6/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a,b, c, localmax;
+Console.WriteLine("Введите 3 числа");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    localmax = a;
+
+}
+else 
+{
+     localmax = b;
+}
+if (localmax < c)
+{
+    localmax = c;    
+}
+
+Console.WriteLine("Максимальное число " + localmax);
