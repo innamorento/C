5d8d0c8371c4cbed56c495341bc8a38e84a817a8
innamorento <innamorento@gmail.com>
2022-12-11 21:04:56 +0300

    Добаавлено решение задачи с рассчетом расстояния между двух точек в трехмерном пространстве

---
+++ tast21/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите 3 координаты 1 точки");
+int firstx = Convert.ToInt32(Console.ReadLine());
+int firsty = Convert.ToInt32(Console.ReadLine());
+int firstz = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3 координаты 2 точки");
+int secondx = Convert.ToInt32(Console.ReadLine());
+int secondy = Convert.ToInt32(Console.ReadLine());
+int secondz = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(secondx - firstx,2) + Math.Pow(secondy - firsty,2)+ Math.Pow(secondz - firstz,2));
+Console.Write($" Расстояние между точками  {result} ");
