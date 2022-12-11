c89e97f3c6a8c410d51562b694ddcff5f59ec87e
innamorento <innamorento@gmail.com>
2022-12-11 20:50:45 +0300

    Добаавлено решение задачи с выводом куба числа от 1 до указаного числа

---
+++ tast23/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= x; i++)
+{
+    Console.Write($" Число {i} в кубе = {i*i*i} ");
+}
