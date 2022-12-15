3e08eb18d0686bd97dcc65102c5d3e495338134d
innamorento <innamorento@gmail.com>
2022-12-15 17:16:08 +0300

    Добаавлено решение задачи с вводом ммасива через консоль и его выводом

---
+++ tast29/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[] createnewarray()
+{
+    Console.WriteLine("Введите размер массива");
+    int arraysize = Convert.ToInt32(Console.ReadLine());
+    int[] newarray = new int[arraysize];
+    for (int i = 0; i < arraysize; i++)
+    {
+        Console.WriteLine($"Введите {i} элемент массива");
+        newarray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return newarray;
+}
+
+void printarray(int[] arrayforprint)
+{
+    for (int i = 0; i < arrayforprint.Length; i++)
+    {
+        Console.Write($"{arrayforprint[i]} ");
+    }
+    Console.WriteLine();
+}
+
+//int[] newarray = createnewarray(); 
+printarray(createnewarray()); 
