16841e8ac0541b35a267f93a08f70b2f8ba5c81b
innamorento <innamorento@gmail.com>
2022-12-15 16:59:44 +0300

    Добаавлено решение задачи с возведением числа в заданую степень

---
+++ tast27/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите в какую степень возвести число");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+int mypow(int x, int y)
+{
+    int result = 1;
+    for (int i = 1; i <= y; i++)
+    {
+        result = result * x;
+    }
+    return result;
+}
+
+Console.WriteLine($"число {number} в степени {degree} = {mypow(number, degree)}");
