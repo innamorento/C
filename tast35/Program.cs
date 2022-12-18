11e90800c59f044dca4d6c31db54b6cc8dffdf3b
innamorento <innamorento@gmail.com>
2022-12-18 17:19:52 +0300

    Добаавлено решение задачи с созданием массива и подсчетом чисел входящих в укаазнный диапазон

---
+++ tast35/Program.cs
@@ -0,0 +1,46 @@
+﻿int[] generationrandomarray()
+{
+    Console.WriteLine($"Введите размер массива");
+    int arraylength = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Введите начало диапазона случайных чисел");
+    int randombegin = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Введите конец диапазона случайных чисел");
+    int randomend = Convert.ToInt32(Console.ReadLine());
+    int[] ramdomarray = new int[arraylength];
+    for (int i = 0; i < arraylength; i++)
+    {
+        Random rnd = new Random();
+        ramdomarray[i] = rnd.Next(randombegin,randomend);
+    }
+    return ramdomarray;
+}
+
+int numberbelongstotherange(int[] arraynumbers)
+{
+    Console.WriteLine($"Введите начало диапазона в которое должно входить число");
+    int numberbelongstart = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Введите конец диапазона в которое должно входить число");
+    int numberbelongend = Convert.ToInt32(Console.ReadLine());
+    int counter = 0;
+    for (int i = 0; i < arraynumbers.Length; i++)
+    {
+        if (arraynumbers[i] > numberbelongstart & arraynumbers[i] < numberbelongend) 
+        {
+            counter ++;
+        }
+    }
+    return counter;
+}
+
+void printarray(int[] arrayforprint)
+{
+    for (int i = 0; i < arrayforprint.Length; i++)
+    {
+        Console.Write($"{arrayforprint[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] newarray = generationrandomarray();
+printarray(newarray);
+Console.WriteLine($"Четных чисел принадлежащих диапазону {numberbelongstotherange(newarray) }");
