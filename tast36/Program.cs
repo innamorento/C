13a505e65824052487b1d89c4541bdcb0b276c06
innamorento <innamorento@gmail.com>
2022-12-18 17:30:43 +0300

    Добаавлено решение задачи с вычислением суммы всех нечентных элементов массива

---
+++ tast36/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] generationrandomarray()
+{
+    Console.WriteLine($"Введите размер массива");
+    int arraylength = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Введите начало диапазона случайных чисел");
+    int randombegin = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Введите конец диапазона случайных чисел");
+    int randomend = Convert.ToInt32(Console.ReadLine());
+    int[] ramdomarray = new int[arraylength];
+    for (int i = 0; i < arraylength; i++)
+    {
+        Random rnd = new Random();
+        ramdomarray[i] = rnd.Next(randombegin,randomend);
+    }
+    return ramdomarray;
+}
+
+int sumoddelement(int[] arraynumbers)
+{
+    int sum = 0;
+    for (int i = 1; i < arraynumbers.Length; i+=2)
+    {
+        sum += arraynumbers[i];
+    }
+    return sum;
+}
+void printarray(int[] arrayforprint)
+{
+    for (int i = 0; i < arrayforprint.Length; i++)
+    {
+        Console.Write($"{arrayforprint[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] newarray = generationrandomarray();
+printarray(newarray);
+Console.WriteLine($"Сумма нечетных элементов = {sumoddelement(newarray)}");
