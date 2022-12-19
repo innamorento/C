d5d33be687446ca1c3a5f049897ff8a6cf19ac9e
innamorento <innamorento@gmail.com>
2022-12-19 16:02:51 +0300

    Добаавлено решение задачи с вводом чисел до указания стоп слова и ссумой всех положительных введеных чисел

---
+++ tast41/Program.cs
@@ -0,0 +1,24 @@
+﻿int sumnumbers()
+{
+    Console.WriteLine("Введите стоп слово для завершения ввода ряда чисел");
+    string strstopwod = Console.ReadLine();
+    int sum = 0;
+    bool exit = true; 
+    while (exit)
+    {
+        Console.WriteLine($"Введите число или слово {strstopwod} для завершения ввода чисел");
+        string temp = Console.ReadLine();
+        if (temp ==  strstopwod)
+        {
+            exit = false;
+        }
+        else if ( Convert.ToInt32(temp) > 0)
+
+        {
+           sum += Convert.ToInt32(temp); 
+        }
+    }
+    return sum;
+}
+
+ Console.WriteLine($"Сумма положительных чисел =  {sumnumbers()}");
