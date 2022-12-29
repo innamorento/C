a0a1c548d9986016b838857a7c294159d7b228fa
innamorento <innamorento@gmail.com>
2022-12-29 11:28:18 +0300

    Добаавлено решение задачи вычислением суммы в интервле чисел

---
+++ tast66/Program.cs
@@ -0,0 +1,13 @@
+﻿int SumNaturalNumbersInInterval(int startInterval, int stopInterval, int sum)
+{
+    
+    if (startInterval >  stopInterval) return sum;
+    sum = SumNaturalNumbersInInterval(startInterval +1, stopInterval, sum + startInterval);
+    return sum;
+}
+int sum = 0;
+Console.WriteLine("Введите начало интервала");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конец интервала");
+int finish = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine($"Сумма натуральных чисел в интервале от {start} до {finish} = {SumNaturalNumbersInInterval(start, finish, sum)}");
